de3775565c08b772a1d96e3a9f8bded0fe0bc979
Léon Tran-Van <ephalys@live.fr>
2022-08-29 20:04:42 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Ambient</title>
+                <meta name="description" content="Gradients generator" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>
+                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                </h1>
+            </main>
+        </>
+    )
+}
